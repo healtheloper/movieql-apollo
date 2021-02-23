eab9b512510fa0eb38bb500e2f2cbd0941c18cd5
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-02-23 22:07:45 +0900

    #1.1 GET MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,21 @@
-export default () => "Home";
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
